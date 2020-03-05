2d9cd872ce93503a281adabfcde6c6999255d77a
jpark2320 <jpark2320@gmail.com>
2020-03-05 16:07:18 -0500

    Initiate Spring Boot project working with SQL database

---
+++ src/main/java/com/jaehong/springbootsql/SpringBootSqlApplication.java
@@ -0,0 +1,13 @@
+package com.jaehong.springbootsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaehong/springbootsql/SpringBootSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehong.springbootsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
